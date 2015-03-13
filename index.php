ba639dcc8f56c3a159bb4ebb5d3235a06d8ef78c
lgmclaughlin <lgmclaughlin905@gmail.com>
2015-03-12 21:56:08 -0700

    Added basic PHP home page and set up Bootstrap with Sass.

---
+++ index.php
@@ -0,0 +1,23 @@
+<DOCTYPE! html>
+<head>
+	<meta name="description" content="My personal website. Enjoy!"/>
+	<meta name="author" content="Lucas McLaughlin"/>
+	<meta charset="UTF-8"/>
+
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+	<div class="wrapper">
+		<!-- Nav bar -->
+			<!-- Projects, Art, Music, About -->
+
+		<!-- Welcome title -->
+		<h1>Welcome!</h1>
+
+		<!-- Page description -->
+
+		<!-- Three boxes with pictures and text, link to pages -->
+
+	</div>
+</body>
