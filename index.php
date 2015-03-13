d58f060d72866ab59002fb45c67b4748415d5d62
lgmclaughlin <lgmclaughlin905@gmail.com>
2015-03-13 11:01:33 -0700

    Added navbar styling and Sass variables for auto resizing. Changed containers to have grey background

--- index.php
+++ index.php
@@ -10,34 +10,36 @@
 </head>
 
 <body>
-	<!-- Nav bar -->
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a href="#" class="navbar-brand">
-					<img src="assets/images/sig.png" alt="Lucas McLaughlin logo" class="brand"/>
-				</a>
-			</div> <!-- Close navbar-header -->
-			<div class="collapse navbar-collapse" id="navbar">
-				<ul class="nav navbar-nav">
-					<?php 
-						$nav = new NavBar();
-						echo $nav->generateNav();
-					?>
-				</ul>
-			</div> <!-- Close navbar-collapse -->
-		</div> <!-- Close container-fluid -->
-	</nav>
 
-	
+	<!-- Top container div for whitespace -->
+	<div class="container top-container">
+	</div>
 
+	<!-- Main container div -->
 	<div class="container">
-		
+		<!-- Nav bar -->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="#" class="navbar-brand">
+						<img src="assets/images/sig.png" alt="Lucas McLaughlin logo" class="brand"/>
+					</a>
+				</div> <!-- Close navbar-header -->
+				<div class="collapse navbar-collapse" id="navbar">
+					<ul class="nav navbar-nav">
+						<?php 
+							$nav = new NavBar();
+							echo $nav->generateNav();
+						?>
+					</ul>
+				</div> <!-- Close navbar-collapse -->
+			</div> <!-- Close container-fluid -->
+		</nav>
 
 		<!-- Welcome title -->
 		<h1>Welcome!</h1>
