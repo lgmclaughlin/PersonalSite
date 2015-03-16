9edb466bc3b59c4ecdc5a66a375a8656b7a36134
lgmclaughlin <lgmclaughlin905@gmail.com>
2015-03-15 21:29:29 -0700

    Created icons for social media pages and added them to the home page as links.

--- home.php
+++ home.php
@@ -2,4 +2,21 @@
 	<p class="name">Lucas McLaughlin</p>
 	<p class="title">Web and software developer</p>
 	<p class="hobbies">Musician and artist</p>
+	<div class="social-icons">
+		<a href="https://www.facebook.com/McLovin905">
+			<img class="social-icon" src="assets/images/FB-icon.png" alt="Facebook icon, linked to my Facebook profile." />
+		</a>
+		<a href="https://www.facebook.com/lucasmband">
+			<img class="social-icon" src="assets/images/FB-pages-icon.png" alt="Facebook pages icon, linked to my Facebook page." />
+		<a/>
+		<a href="https://www.youtube.com/user/DormRoomSerenade">
+			<img class="social-icon" src="assets/images/YT-icon.png" alt="YouTube icon, linked to my YouTube channel" />
+		</a>
+		<a href="https://www.linkedin.com/in/lucasmclaughlin">
+			<img class="social-icon" src="assets/images/LI-icon.png" alt="LinkedIn icon, linked to my LinkedIn profile" />
+		</a>
+		<a href="https://github.com/lgmclaughlin">
+			<img class="social-icon" src="assets/images/GH-icon.png" alt="GitHub icon, linked to my GitHub page.">
+		</a>
+	</div>
 </div>
