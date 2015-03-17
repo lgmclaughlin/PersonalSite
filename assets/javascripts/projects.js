91878c0eb32f81a7a9c5389456129faeb4e37c98
lgmclaughlin <lgmclaughlin905@gmail.com>
2015-03-16 23:05:13 -0700

    Finished functionality for the Projects page, including Javascript and styling. Now to add content

--- assets/javascripts/projects.js
+++ assets/javascripts/projects.js
@@ -10,14 +10,14 @@ function setUpContent(clickedBtn) {
 		// Get the button that was not clicked
 		var otherBtn = $('#current-btn');
 		// Get the div corresponding to the clicked button
-		var clickedDiv = $('#past-div');
+		var clickedDiv = $('#past-prj-div');
 		// Get the other div corresponding to the unclicked button
-		var otherDiv = $('#current-div');
+		var otherDiv = $('#current-prj-div');
 	} else {
 		// Reverse all the values
 		var otherBtn = $('#past-btn');
-		var clickedDiv = $('#current-div');
-		var otherDiv = $('#past-div');
+		var clickedDiv = $('#current-prj-div');
+		var otherDiv = $('#past-prj-div');
 	}
 	
 	if (clickedBtn.hasClass('active')) {
@@ -25,17 +25,34 @@ function setUpContent(clickedBtn) {
 		clickedBtn.removeClass('active');
 		// Update the button div to the correct class
 		$('#past-current-btns').removeClass('btns-active').addClass('btns-inactive');
-		// Unhid the GitHub link
+		// Unhide the GitHub link
 		$('#git-hub-link').slideDown();
+		// Hide the correct div, past or current
+		clickedDiv.slideUp();
 	} else {
 		// Otherwise, always remove the active state
 		// from the other element and add it to the new one
+
+		// Check if there was already an active element
+		var activeFlag = 0;
+		if (otherBtn.hasClass('active')) {
+			activeFlag = 1;
+		}
+		// Remove from unclicked button, add to clicked
 		otherBtn.removeClass('active');
 		clickedBtn.addClass('active');
 		// Inform the div that one of the buttons is active
 		$('#past-current-btns').removeClass('btns-inactive').addClass('btns-active');
 		// Hide the GitHub link
 		$('#git-hub-link').slideUp();
+		if (activeFlag) {
+			// Fade out open div and fade in clicked
+			otherDiv.fadeOut(300);
+			clickedDiv.fadeIn(300);
+		} else {
+			// Display the correct div, past or current
+			clickedDiv.slideDown();
+		}
 	}
 }
 
