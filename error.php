2057b7c0eb8a3111ebc7c8a5f86ed626b3e132a8
lgmclaughlin <lgmclaughlin905@gmail.com>
2015-03-23 21:13:32 -0700

    Error page is now complete. Made minor UI changes to Music page.

--- error.php
+++ error.php
@@ -1,2 +1,9 @@
-<h1>Oops!</h1>
+<div class="container">
+    <h1 class="error-title">Oops...</h1>
+    <p class="content-p error-p">
+        The page you are looking for returned a status of 404. 
+        <br />
+        In other words, it wasn't found. Sorry about that!
+    </p>
+</div>
 </div> <!-- close the main-container div from index.php to allow modal blur -->
