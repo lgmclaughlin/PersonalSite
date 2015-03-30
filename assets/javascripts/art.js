22fbf98647d2673c9b2a528bbd825deb44068c36
lgmclaughlin <lgmclaughlin905@gmail.com>
2015-03-29 19:56:21 -0700

    Checked for Safari on Art and disabled modals since they do not relocate properly.

--- assets/javascripts/art.js
+++ assets/javascripts/art.js
@@ -2,28 +2,31 @@ $(document).ready(function() {
     // Unhide footer
     $('#footer').css('visibility', 'visible');
 
-    /**
-     * Select all modals and add modal functionality
-     */
-    $('.art-sketch').click(function() {
-        var imgSrc = $(this).attr('src');
-        var html = '<img src="' + imgSrc + '" class="modal-active" />\n';
-        var insertDiv = $('#active-modal-div');
+    // Check if Safari
+    if (!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/)) {
+        /**
+         * Select all modals and add modal functionality
+         */
+        $('.art-sketch').click(function() {
+            var imgSrc = $(this).attr('src');
+            var html = '<img src="' + imgSrc + '" class="modal-active" />\n';
+            var insertDiv = $('#active-modal-div');
 
-        // Empty the modal div, insert the clicked image,
-        // unhide the modal div, and blur the background
-        insertDiv.empty();
-        insertDiv.append(html);
-        insertDiv.removeClass('hidden');
-        $('#main-container').addClass('blur');
-    });
+            // Empty the modal div, insert the clicked image,
+            // unhide the modal div, and blur the background
+            insertDiv.empty();
+            insertDiv.append(html);
+            insertDiv.removeClass('hidden');
+            $('#main-container').addClass('blur');
+        });
 
-    $('#active-modal-div').click(function () {
-        var insertDiv = $('#active-modal-div');
+        $('#active-modal-div').click(function () {
+            var insertDiv = $('#active-modal-div');
 
-        // Empty the modal div, hide it, and remove background blur
-        insertDiv.empty();
-        insertDiv.addClass('hidden');
-        $('#main-container').removeClass('blur');
-    });
+            // Empty the modal div, hide it, and remove background blur
+            insertDiv.empty();
+            insertDiv.addClass('hidden');
+            $('#main-container').removeClass('blur');
+        });
+    }
 });
